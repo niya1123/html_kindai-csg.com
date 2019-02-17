a324ea62b6daaa94461d2600134e96e3036f2e03
niya <yoheikurioka@gmail.com>
2019-02-17 12:08:52 +0900

    エディタのとこだけやったけど、フル画面表示したときに右側に余白がるけど、それが消せない

---
+++ js/ace_view.js
@@ -0,0 +1,6 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.setFontSize(14);
+editor.getSession().setMode("ace/mode/html");
+editor.getSession().setUseWrapMode(true);
+editor.getSession().setTabSize(2);
